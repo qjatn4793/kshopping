e26d4cbe4a7b3d28f8c0d228278217c25efae583
qjatn4793 <qjatn4793@naver.com>
2022-06-16 10:59:12 +0900

    2022-06-16 김범수
    1. js lib 추가 (인터넷 없이도 동작 가능하도록 수정)
    
    #### 해야할 것 ####
    1. 상품 상세 보기영역에 있는 검색 및 카테고리 영역 개발필요

--- src/test/java/com/shopping/kshopping/KshoppingApplicationTests.java
+++ src/test/java/com/shopping/kshopping/KshoppingApplicationTests.java
@@ -1,6 +1,6 @@
 package com.shopping.kshopping;
 
-import org.junit.jupiter.api.Test;
+/*import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,4 +10,4 @@ class KshoppingApplicationTests {
     void contextLoads() {
     }
 
-}
+}*/
